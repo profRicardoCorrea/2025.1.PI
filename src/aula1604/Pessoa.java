df3dfd8aeb512cbb455734db86247ccc19afcfe9
profRicardoCorrea <ricardo.correia@unifacol.edu.br>
2025-04-16 21:09:45 -0300

    pessoa

---
+++ src/aula1604/Pessoa.java
@@ -0,0 +1,28 @@
+package aula1604;
+
+public class Pessoa {
+	public String nomeCompleto;
+	public String cpf;
+	public int idade;
+	
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	
+}
